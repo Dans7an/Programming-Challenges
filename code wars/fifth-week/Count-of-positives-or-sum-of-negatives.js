0d166793785a3b0d6df50fda1a660e696b1b9260
Dans7an <kdanstanmark@gmail.com>
2021-03-21 01:21:00 -0400

    A few

---
+++ code wars/fifth-week/Count-of-positives-or-sum-of-negatives.js
@@ -0,0 +1,31 @@
+/* 
+Given an array of integers.
+
+Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers.
+
+If the input array is empty or null, return an empty array.
+
+Example
+For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
+*/
+
+function countPositivesSumNegatives(input) {
+    let pos = 0
+    let neg = 0
+    let arr3 = []
+    if(input == null || input.length == 0){
+      return []
+      } 
+    for(let i = 0; i < input.length; i++){
+      if(Math.sign(input[i]) == 1){
+        pos++
+      } else if(Math.sign(input[i]) == -1){
+        neg += input[i]
+      }
+    }
+  
+  arr3.push(pos)
+  arr3.push(neg)
+  return arr3
+
+    }
