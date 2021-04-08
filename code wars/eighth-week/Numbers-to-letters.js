9188f0f3216dcbde96fb0c9dd6e604c518c30ce7
Dans7an <kdanstanmark@gmail.com>
2021-04-07 23:02:22 -0400

    Numbers to letters

---
+++ code wars/eighth-week/Numbers-to-letters.js
@@ -0,0 +1,16 @@
+/* Given an array of numbers (in string format), you must return a string. The numbers correspond to the letters of the alphabet in reverse order: a=26, z=1 etc. You should also account for '!', '?' and ' ' that are represented by '27', '28' and '29' respectively.
+
+All inputs will be valid.
+
+*/
+
+function switcher(x){
+  let alphabet = [' ','?','!','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+  let newArr = alphabet.reverse()
+  let arr = []
+
+  for(let num of x){
+    arr.push(newArr[num - 1])
+  }
+  return arr.join('')
+}
